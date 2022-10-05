36f09b78230cb03a4d0137f4878572eef2614b37
hzhang20902 <85701939+hzhang20902@users.noreply.github.com>
2022-10-05 19:28:39 -0400

    added constants and components

--- screens/Home.js
+++ screens/Home.js
@@ -1,9 +1,26 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, View, SafeAreaView, FlatList } from 'react-native'
+import { useState } from 'react'
+
+import { COLORS, NFTData } from '../constants'
+import { NFTCard, HomeHeader, FocusedStatusBar } from '../components'
 
 const Home = () => {
   return (
-    <Text>Home</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+        <FocusedStatusBar background={COLORS.primary} />
+
+        <View style={{ flex: 1 }}>
+            <View style={{ zIndex: 0 }}>
+                <FlatList 
+                data={NFTData}
+                renderItem={({ item }) => <Text>{item.name}</Text>}
+                keyExtractor={(item) => item.id}
+                showsVerticalScrollIndicator={false}
+                ListHeaderComponent={<HomeHeader />} />
+            </View>
+        </View>
+    </SafeAreaView>
   )
 }
 

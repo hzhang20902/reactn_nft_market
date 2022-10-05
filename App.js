7cae1302c8359523f08fc671120f2a8fa229bef9
hzhang20902 <85701939+hzhang20902@users.noreply.github.com>
2022-10-05 18:43:01 -0400

    initial

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Whatup! I'm Jared, I'm 19, and I never learned how to fuckin read!</Text>
       <StatusBar style="auto" />
     </View>
   );
